9f686777500b2d3434718e2c728fab712dc0306b
Riccardo Piredda <127589992+Nemonek@users.noreply.github.com>
2024-02-10 14:57:46 +0100

    Initial commit

---
+++ Piredda.Riccardo.4i.rubricaWPF/Contatto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piredda.Riccardo._4i.rubricaWPF
+{
+    public enum TipoContatto
+    {
+        email,
+        numero,
+        web,
+        instagram,
+        nessuno
+    }
+    public class Contatto
+    {
+        private int _PK;
+        private string _nome;
+        private TipoContatto _tipo;
+        private string _valore;
+
+        public int PK { get => this._PK; }
+        public string Nome { get => this._nome; }
+        public TipoContatto Tipo { get => this._tipo; }
+        public string Valore { get => this._valore; }
+
+        public Contatto(string r)
+        {
+            string[] values = r.Split(';');
+            this._PK = (int.TryParse(values[0], out int n)) ? n : 0 ;
+            this._nome  = values[1];
+            this._tipo = (Enum.TryParse(values[2], out TipoContatto t)) ? t : TipoContatto.nessuno;
+            this._valore = values[3];
+        }
+    }
+}
---
+++ Piredda.Riccardo.4i.rubricaWPF/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
+
+namespace Piredda.Riccardo._4i.rubricaWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<Contatto> _tutteLeInformazioni;
+        private List<Contatto> _itemSourceContatti;
+        private List<Persona> _itemSourcePersone;
+
+
+        public List<Contatto> ItemSourceContatti { get => this._itemSourceContatti; }
+        public List<Persona> ItemSourcePersone { get => this._itemSourcePersone; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            // Tutta la roba va inserita all'interno dell'evento Loaded della MainWindow.
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            this._itemSourceContatti = new();
+            this._tutteLeInformazioni = new();
+            this._itemSourcePersone = new();
+            // Siccome l'apertura di un file può causare un eccezione per diversi motivi
+            try
+            {
+                using ( StreamReader sr = new( "Contatti.csv" ) )
+                {
+                    sr.ReadLine();
+                    string riga = string.Empty;
+                
+                    while (!sr.EndOfStream)
+                        this._tutteLeInformazioni.Add(new Contatto(sr.ReadLine()));
+
+                    sr.Close();
+                }
+                StatusBar.Text = $"Trovati {this._tutteLeInformazioni.Count} contatti";
+
+                using ( StreamReader sr = new( "Persone.csv" ) )
+                {
+                    sr.ReadLine();
+                    string riga = string.Empty;
+
+                    while (!sr.EndOfStream)
+                        this._itemSourcePersone.Add(new Persona(sr.ReadLine()));
+
+                    sr.Close();
+                }
+                StatusBar.Text = $"Trovati {StatusBar.Text} contatti e {this._itemSourcePersone.Count} persone";
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+
+            GrigliaContatti.ItemsSource = this.ItemSourceContatti;
+            GrigliaPersone.ItemsSource = this.ItemSourcePersone;
+        }
+
+        private void GrigliaContatti_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            //StatusBar.Text = $"Contatto selezionato: {((Contatto)e.AddedItems[0]).Nome}";
+        }
+        private void GrigliaPersone_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            this._itemSourceContatti.Clear();
+            Persona p = e.AddedItems[0] as Persona;
+            this._itemSourceContatti = new List<Contatto>(this._tutteLeInformazioni.Where(t => t.PK == p.PK));
+
+            GrigliaContatti.ItemsSource = this.ItemSourceContatti;
+        }
+    }
+}
